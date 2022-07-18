9b7c22a2cb69c69f240bc0437481f22f82be35fc
Winford David <87101969+winforddavid@users.noreply.github.com>
2022-07-18 19:36:12 +0000

    tetsing js scripts

---
+++ _nss/_sandbox/sbox2/javascript.js
@@ -0,0 +1,10 @@
+//JS comments for my application
+//window.alert("This is using the Window function to display a message")
+//document.write("This is using the document.write function to display a message");
+//console.log("would typically be used for capturing the erros within the consol for the dev!");
+//alert("A popup message in a box");
+//about:blank
+alert("A popup message in a box - My Awesome Application");
+console.log("would typically be used for capturing the erros within the consol for the dev!");
+document.write("This is using the document.write function to display a message");
+window.alert("This is using the Window.alert function to display a message");
